a9f5d0e1df71fcdc4ee3f137462dd54c85f0c7b9
Greg Nwosu <greg.nwosu@gmail.com>
2014-03-28 23:40:17 +0000

    First commit

---
+++ src/test/java/GridSteps.java
@@ -0,0 +1,17 @@
+import com.inkglobal.techtest.BerlinClock;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: greg
+ * Date: 28/03/14
+ * Time: 22:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class GridSteps {
+
+    private BerlinClock clk;
+
+
+
+
+}
